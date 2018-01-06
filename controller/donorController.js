0f338a38a2b82d994f3d3ad85f5e1331d6a27f2b
ramola <sramola5@gmail.com>
2018-01-06 14:46:29 +0530

    Created Route to Enter Data

--- controller/donorController.js
+++ controller/donorController.js
@@ -1,14 +1,40 @@
 const donor = require("../models/donor");
 
-const createDonor = function(req, res){};
+const createDonor = function(req, res, next){
+  const user = new donor({
+    name: req.body.name,
+    address: req.body.address,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    bloodGroup: req.body.bloodGroup,
+    registerationNumber: req.body.registerationNumber
+  });
+  user.save((err, user)=>{
+    if(err){ return console.log(err);}
+    const response = {
+      status : true,
+      message : "Succesfully Created The Donor Profile ",
+      id : user._id
+    };
+    console.log(user);
+    res.json(response);
+  })
+};
 
-const donorDetail = function(req, res){};
+const donorDetail = function(req, res, next){
+  res.send("Not Yet Implemented");
+};
 
-const deleteDonor = function(req, res){};
+const deleteDonor = function(req, res, next){};
 
-const updateDonor = function(req, res){};
+const updateDonor = function(req, res, next){};
 
-const searchDonor = function (req, res) {};
+const searchDonor = function (req, res, next) {};
+
+const createDonorForm = function(req, res, next) {
+  res.render("donorform");
+};
+
 
 module.exports = {
   createDonor : createDonor,
@@ -16,4 +42,5 @@ module.exports = {
   deleteDonor : deleteDonor,
   updateDonor : updateDonor,
   searchDonor : searchDonor,
+  createDonorForm : createDonorForm,
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const donorController = require("../controller/donorController");
+const { check, validationResult } = require('express-validator/check');
 
-router.get("/:id", donorController.donorDetail);
+router.get("/donor/:id", donorController.donorDetail);
 
 router.get("/search", donorController.searchDonor);
 
-router.post("/create", donorController.createDonor);
+router.post("/create",[
+  check("name", "Name of Donor Must not be Empty").isLength({min: 1}),
+  check("address", "Address of Donor Must not be Empty").isLength({min: 1}),
+  check("email", "Email of Donor Must not be Empty").isLength({min: 1}),
+  check("phoneNumber", "Phone Number of Donor Must not be Empty").isLength({min: 1}),
+  check("bloodGroup", "Blood Group of Donor Must not be Empty").isLength({min: 1}),
+],donorController.createDonor);
 
 router.put("/update/:id", donorController.updateDonor);
 
 router.delete("/remove/:id", donorController.deleteDonor);
+
+router.get("/create", donorController.createDonorForm);
+
+router.get("/", function(req, res, next){
+  res.send("Profile of Donor");
+});
 
 module.exports = router;
